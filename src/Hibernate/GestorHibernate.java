2c5a777c4740635a0e487dcdc7faa64f70ef3151
nicdu <nicdu@DESKTOP-KQ61S2S>
2021-01-04 14:45:52 -0300

    funcionalidad base administracion

---
+++ src/Hibernate/GestorHibernate.java
@@ -0,0 +1,105 @@
+package Hibernate;
+import java.awt.Component;
+import java.util.*;
+import javax.swing.JOptionPane;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.*;
+
+public class GestorHibernate extends HibernateUtil {
+
+    private Transaction tx;
+    
+    public void eliminarObjeto(Object objeto){
+        try{   
+            Session s = HibernateUtil.getSession();
+            Transaction tx = s.beginTransaction();
+            s.delete(objeto); 
+            tx.commit();
+        } catch(HibernateException ex){
+            System.out.println("Repositorio.eliminarObjeto(Object objeto)"+ex);
+            JOptionPane.showMessageDialog((Component)null,"El elemento no se puede eliminar:"+ex.getMessage(),"Error",0);
+            getTx().rollback();
+        }
+    }
+
+    public void guardarObjeto(Object objeto){
+        try{
+        Session s = HibernateUtil.getSession();
+        Transaction tx = s.beginTransaction();
+        s.save(objeto);        
+        tx.commit();
+        System.out.println(" guardaractualizarObjeto() " +objeto.getClass()+": "+objeto.toString());  
+        } catch(HibernateException ex){
+            System.out.println("error "+ex);
+            System.out.println("Repositorio.guardarObjeto(Object objeto)"+objeto.getClass()+": "+objeto.toString()+ex);
+            getTx().rollback();
+        }
+    }
+
+    public boolean actualizarObjeto(Object objeto){
+        Session s = HibernateUtil.getSession();
+        Transaction tx = s.beginTransaction();
+        try{
+            s.update(objeto);
+            tx.commit();
+            System.out.println(" actualizarObjeto() " +objeto.getClass()+": "+objeto.toString());
+            return true;
+        }catch(HibernateException e){
+            JOptionPane.showMessageDialog(null, e);
+            tx.rollback();
+            JOptionPane.showMessageDialog(null, "No se pueden guardar los datos. \nLos mismos han sido modificados por otra persona.");
+            return false;
+        }
+    }
+    
+    public Transaction getTx() {
+        return tx;
+    }
+
+    public void setTx(Transaction tx) {
+        this.tx = tx;
+    }
+
+    public List listarClase(Class clase){
+        Criteria crit = getSession().createCriteria(clase);
+        return crit.list();
+    }
+
+    public List listarClase(Class clase,String atributoOrden, boolean estado,int max){
+        Criteria crit = getSession().createCriteria(clase).addOrder(Order.asc(atributoOrden))
+                        .add (Restrictions.eq("estado",estado)).setMaxResults(max);
+        return crit.list();
+    }
+    public List listarClase(Class clase, boolean estado,int max){
+        Criteria crit = getSession().createCriteria(clase).add (Restrictions.eq("estado",estado)).setMaxResults(max);
+        return crit.list();
+    }
+
+    public List listarClase(Class clase,String atributoOrden){
+        Criteria crit = getSession().createCriteria(clase).addOrder(Order.asc(atributoOrden));
+        return crit.list();
+    }
+
+    public List listarUltimo(Class clase) {
+           Criteria crit = getSession().createCriteria(clase) 
+                        .addOrder(Order.desc("codigo"));
+        return crit.list();
+    }
+
+    public List listarClaseCodigo(Class clase, int valor){        
+        Criteria crit = getSession().createCriteria(clase)
+            .add( Restrictions.eq("codigo", valor))
+             .add( Restrictions.eq("estado", 0));
+        return crit.list();
+    }  
+    
+    public Object listarClaseCodigoUnique(Class clase, int valor){
+        Criteria crit = getSession().createCriteria(clase)
+            .add( Restrictions.eq("codigo", valor))
+            .add( Restrictions.eq("estado", 0));
+        return crit.uniqueResult();
+    }
+}
---
+++ src/Hibernate/HibernateUtil.java
@@ -0,0 +1,59 @@
+package Hibernate;
+import java.awt.HeadlessException;
+import javax.swing.JOptionPane;
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+public class HibernateUtil  {
+    public static SessionFactory sessionFactory;
+    public static Session session;
+
+    public static void inicializar() {
+        try {
+           Configuration conf = new Configuration(); 
+           try{
+                conf.setProperty("hibernate.connection.driver_class","org.postgresql.Driver");
+                conf.setProperty("hibernate.dialect","org.hibernate.dialect.PostgreSQLDialect");                 
+                conf.setProperty("hibernate.connection.url","jdbc:postgresql://localhost:5432/TPI-PA");
+            }
+            catch(Exception e){
+                JOptionPane.showMessageDialog(null, "Error de Base de Datos N° 2001");
+            }
+            
+            conf.setProperty("hibernate.connection.username","postgres");
+            conf.setProperty("hibernate.connection.password","00");
+            conf.setProperty("hibernate.connection.pool_size","10");                          
+            conf.setProperty("hibernate.hbm2ddl.auto","update");
+            conf.addPackage("Modelos");
+            
+            conf.addAnnotatedClass(Modelos.Pedidos.Calificacion.class);
+            conf.addAnnotatedClass(Modelos.Categoria_Producto.CategoriaProducto.class);
+            conf.addAnnotatedClass(Modelos.Usuarios.Clientes.Cliente.class);
+            conf.addAnnotatedClass(Modelos.Rubros.Rubro.class);
+            conf.addAnnotatedClass(Modelos.Usuarios.Usuario.class);
+            conf.addAnnotatedClass(Modelos.TipoObjeto.class);
+            
+            conf.addAnnotatedClass(Modelos.Usuarios.Administradores.Administrador.class);
+            conf.addAnnotatedClass(Modelos.Usuarios.Comercios.Comercio.class);
+            conf.addAnnotatedClass(Modelos.Productos.Producto.class);
+            
+            conf.addAnnotatedClass(Modelos.Pedidos.DetallePedido.class);
+            conf.addAnnotatedClass(Modelos.Pedidos.Pedido.class);
+            
+            try {
+                    sessionFactory = conf.buildSessionFactory();
+                    session=sessionFactory.openSession();
+                }
+                catch(HibernateException e){
+                    JOptionPane.showMessageDialog(null, e);
+                }
+        } catch (HeadlessException | MappingException ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static Session getSession()
+    throws HibernateException {
+        return session;
+    }
+}
